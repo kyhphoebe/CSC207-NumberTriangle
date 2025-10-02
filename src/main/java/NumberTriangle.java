3609dd0085d461661a0c1e2d988a4ff02b3c83b7
kyhphoebe <kyhphoebe@gmail.com>
2025-10-01 20:21:32 -0400

    Implemented loadTriangle

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,8 @@
 import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -88,7 +92,6 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
         return -1;
     }
 
@@ -109,25 +112,30 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
-
-        // will need to return the top of the NumberTriangle,
-        // so might want a variable for that.
-        NumberTriangle top = null;
+        List<List<NumberTriangle>> triangle = new ArrayList<>();
 
         String line = br.readLine();
         while (line != null) {
 
-            // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
-
-            // TODO process the line
+            String[] parts = line.split(" ");
+            List<NumberTriangle> row = new ArrayList<>();
+            for (String p : parts) {
+                row.add(new NumberTriangle(Integer.parseInt(p)));
+            }
+            triangle.add(row);
 
             //read the next line
             line = br.readLine();
         }
         br.close();
+
+        NumberTriangle top = triangle.get(0).get(0);
+        for (int i = 0; i < triangle.size() - 1; i++) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                triangle.get(i).get(j).left = triangle.get(i + 1).get(j);
+                triangle.get(i).get(j).right = triangle.get(i + 1).get(j + 1);
+            }
+        }
         return top;
     }
 
